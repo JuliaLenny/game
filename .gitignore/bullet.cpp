2116788b827a8d8aa27b6dced15097ad50bb2c68
JuliaLenny <31353101+JuliaLenny@users.noreply.github.com>
2017-11-27 22:06:02 +0300

    Create bullet.cpp

---
+++ .gitignore/bullet.cpp
@@ -0,0 +1,28 @@
+#include "bullet.h"
+#include "wall.h"
+
+
+/////////////////////////////////////
+// Class Bullet
+Bullet::Bullet()
+{
+	m_type = GameObjectType_Bullet;
+	m_ownerType = GameObjectType_None;
+
+	setTextureRect(kBulletImage);
+}
+
+
+void Bullet::render(sf::RenderWindow* rw)
+{
+	GameObject::render(rw);
+}
+
+void Bullet::intersect(GameObject* object)
+{
+	// Destroy self
+	setHealth(0);
+
+	// Do damage to target
+	object->doDamage(1);
+}
