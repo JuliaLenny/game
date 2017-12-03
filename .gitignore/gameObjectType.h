7d6091a94753375ccd600cccfc39bf6aa21ce68e
JuliaLenny <31353101+JuliaLenny@users.noreply.github.com>
2017-12-04 02:08:12 +0300

    Update gameObjectType.h

--- .gitignore/gameObjectType.h
+++ .gitignore/gameObjectType.h
@@ -1,14 +1,11 @@
 #pragma once
-
-
-/////////////////////////////////////
 // Functions
 enum GameObjectType
 {
 	GameObjectType_None,
-	GameObjectType_Tank,
-	GameObjectType_TankPlayer,
-	GameObjectType_TankEnemy,
+	GameObjectType_Ghost,
+	GameObjectType_GhostPlayer,
+	GameObjectType_GhostEnemy,
 	GameObjectType_Wall,
 	GameObjectType_Bullet,
 	GameObjectType_EnemySpawner,
