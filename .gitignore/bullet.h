8c6044626e7ae51a4f36de9acfd48f23c5fa8f83
JuliaLenny <31353101+JuliaLenny@users.noreply.github.com>
2017-12-04 02:00:14 +0300

    Update bullet.h

--- .gitignore/bullet.h
+++ .gitignore/bullet.h
@@ -2,7 +2,7 @@
 #include "gameObject.h"
 #include "level.h"
 
-// Class Tank
+// Class Ghost
 class Bullet
 	: public GameObject
 {
@@ -10,12 +10,10 @@ public:
 	Bullet();
 
 	virtual void render(sf::RenderWindow* rw);
-
 	virtual void intersect(GameObject* object);
-
 	void setOwnerType(GameObjectType ownerType) { m_ownerType = ownerType; }
 	GameObjectType getOwnerType() { return m_ownerType; }
 
 private:
 	GameObjectType m_ownerType;
-};
+}
