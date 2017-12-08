64b5f2ed6c80ef92f2699fad3f3d04a3768a5915
JuliaLenny <31353101+JuliaLenny@users.noreply.github.com>
2017-12-08 09:04:01 +0300

    Update level.h

--- .gitignore/level.h
+++ .gitignore/level.h
@@ -9,14 +9,13 @@ const int kScreenRows = 40;
 const int kScreenColumns = 80;
 const int kLevelRows = kScreenRows;
 const int kLevelColumns = 70;
-const int kPixelsPerCell = 15;
+const int kPixels = 15;
 
 const unsigned char	CellSymbol_Player = '1';
 const unsigned char	CellSymbol_Ghost = 'T';
 const unsigned char	CellSymbol_BrickWall = '#';
 const unsigned char	CellSymbol_SteelWall = '@';
-const unsigned char	CellSymbol_EnemySpawner = 'S';
-//const char win[] = "You win, motherfucker!!!";
+const unsigned char	CellSymbol_Enemy = 'S';
 
 
 const unsigned char kLevelData0[kLevelRows][kLevelColumns + 1] =
@@ -25,64 +24,64 @@ const unsigned char kLevelData0[kLevelRows][kLevelColumns + 1] =
 	"@S                                @@                              S  @",
 	"@                                 @@                                 @",
 	"@                                 @@                                 @",
-	"@                                          S         @@@             @",
-	"@                                                                    @",
-	"@                                                                    @",
-	"@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                         @",
-	"@                                                                    @",
-	"@                                                                    @",
-	"@                                                                    @",
-	"@                                                                    @",
-	"@            @                                                       @",
+	"@                                          S        @@@@             @",
+	"@                                                   @                @",
+	"@                                                   @                @",
+	"@@@@@@@@@@@@@@@@@@@@@@@@@@@@                        @                @",
+	"@                         @                         @                @",
+	"@                         @                         @                @",
+	"@                         @                          @@@@@@@@        @",
+	"@                         @                                          @",
+	"@            @            @@@@@                                      @",
 	"@            @                                                       @",
 	"@            @                                                       @",
 	"@            @                           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
-	"@   S        @                                                       @",
-	"@            @                                                       @",
-	"@            @                                                       @",
-	"@@@@@@@@@@@@@@@@         @                                           @",
-	"@                        @                                           @",
-	"@                        @                                           @",
-	"@                   @@@@@@             @                             @",
-	"@                   @                  @                             @",
-	"@                   @                  @                             @",
-	"@                   @                  @                      S      @",
-	"@@@@@@@@@@@@@@@@@@@@@                  @                             @",
-	"@             @                        @                             @",
-	"@         1   @                        @                             @",
-	"@             @                        @              @@@            @",
-	"@                                      @              @@@            @",
-	"@                                      @                             @",
-	"@                                      @                             @",
-	"@                                      @                             @",
-	"@             @                        @                             @",
-	"@             @                        @                             @",
-	"@             @                        @                             @",
-	"@             @                        @                             @",
+	"@   S        @                                      @                @",
+	"@            @                                       @               @",
+	"@            @                @                      @               @",
+	"@@@@@@@@@@@@@@@@         @    @                      @               @",
+	"@                        @                           @               @",
+	"@                        @                           @               @",
+	"@                   @@@@@@                            @@@@           @",
+	"@                   @                                    @@          @",
+	"@                   @                                                @",
+	"@                   @                                         S      @",
+	"@@@@@@@@@@@@@@@@@@@@@                    @@                          @",
+	"@             @                         @@                           @",
+	"@         1   @                        @@                            @",
+	"@             @                       @@               @@@           @",
+	"@                                    @@                @@@           @",
+	"@                                   @@                @              @",
+	"@                                  @@                 @              @",
+	"@                                 @@                  @              @",
+	"@             @                  @@                   @              @",
+	"@             @                 @@                    @              @",
+	"@             @               @@                     @               @",
+	"@             @               @                      @               @",
 	"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
 };
 
 
-const sf::IntRect kBrickWallImage = sf::IntRect(0, 3 * kPixelsPerCell, kPixelsPerCell, kPixelsPerCell);
-const sf::IntRect kSteelWallImage = sf::IntRect(kPixelsPerCell, 3 * kPixelsPerCell, kPixelsPerCell, kPixelsPerCell);
+const sf::IntRect kBrickWallImage = sf::IntRect(0, 3 * kPixels, kPixels, kPixels);
+const sf::IntRect kSteelWallImage = sf::IntRect(kPixels, 3 * kPixels, kPixels, kPixels);
 
 // Ghost's data
-const float kGhostFireCooldownTime = 0.5;
-const int kGhostSize = 3;
-const sf::IntRect kGhostImage = sf::IntRect(0, 0, kGhostSize*kPixelsPerCell, kGhostSize*kPixelsPerCell);
+const float kGhostRecoveryTime = 0.5; //FireCooldown воссстановление
+const int kGhostSize = 3; //размер призрака
+const sf::IntRect kGhostImage = sf::IntRect(0, 0, kGhostSize*kPixels, kGhostSize*kPixels);
 
 // Player data
-const int kPlayerHealth = 16;
-const int kPlayerSpeed = 10;
+const int kPlayerHealth = 16; //здоровье
+const int kPlayerSpeed = 10; //скорость
 
-// Enemy data
-const int kEnemiesPerLevel = 24;
-const int kEnemiesPerLevelInOneMoment = 6;
-const int kEnemyHealth = 5;
-const int kEnemySpeed = 5;
-const float kEnemySpawnerSpawnTime = 10.0;
+							 // Enemy data
+const int kEnemiesLevel = 10;  //количество врагов    
+const int kEnemiesLevelInOneMoment = 6; //масимальное кооличество врагов в один момент  
+const int kEnemyHealth = 5; //здоровье врага
+const int kEnemySpeed = 5;  //скорость врагов
+const float kEnemyEmergenceTime = 10.0; // востанновление
 const float kEnemyAIAnalizeTime = 0.5;
 
 // Bullet data
 const float kBulletSpeed = 30.0;
-const sf::IntRect kBulletImage = sf::IntRect(0, 4 * kPixelsPerCell, kPixelsPerCell, kPixelsPerCell);
+const sf::IntRect kBulletImage = sf::IntRect(0, 4 * kPixels, kPixels, kPixels);
